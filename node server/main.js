05de5ee90729022196619eba0a49c0da71895f3f
Schedule93 <cushnirnicolai@gmail.com>
2021-05-29 13:08:17 +0300

    build x0026 delete old project t-o

--- node server/main.js
+++ node server/main.js
@@ -40,64 +40,6 @@ app.post('/quotes', (req, res) => {
 	res.redirect("/crud");
 });
 
-/* Begin Middleware */
-function middleware(){};
-
-var stack = middleware(function(req, res, next) {
-    users.getAll(function(err, users) {
-        if (err) next(err);
-        req.users = users;
-        next();  
-    });
-}, function(req, res, next) {
-    posts.getAll(function(err, posts) {
-        if (err) next(err);
-        req.posts = posts;
-        next();
-    })
-}, function(req, res, next) {
-    req.posts.forEach(function(post) {
-        post.user = req.users[post.userId];
-    });
-
-    res.render("blog/posts", {
-        "posts": req.posts
-    });
-});
-
-app.get("/posts", function(req, res) {
-   stack.handle(req, res); 
-});
-
-app.get("/posts", [
-    function(req, res, next) {
-        users.getAll(function(err, users) {
-            if (err) next(err);
-            req.users = users;
-            next();  
-        });
-    }, function(req, res, next) {
-        posts.getAll(function(err, posts) {
-            if (err) next(err);
-            req.posts = posts;
-            next();
-        })
-    }, function(req, res, next) {
-        req.posts.forEach(function(post) {
-            post.user = req.users[post.userId];
-        });
-
-        res.render("blog/posts", {
-            "posts": req.posts
-        });
-    }
-], function(req, res) {
-   stack.handle(req, res); 
-});
-/* End middleware */
-
-
-
 app.listen(port, function(){
   console.log(`Server running port ${port}`);
 });
